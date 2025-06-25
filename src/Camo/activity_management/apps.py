acbaa622798a9cd32a37a9303111c2913dda36c4
Angel Ordoñez <103274894+SantiagoAngel007@users.noreply.github.com>
2025-06-24 22:47:41 -0500

    chore:Apps for every funtion created

---
+++ src/Camo/activity_management/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ActivityManagementConfig(AppConfig):
+    name = 'activity_management'
---
+++ src/Camo/autentification/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AutentificationConfig(AppConfig):
+    name = 'autentification'
